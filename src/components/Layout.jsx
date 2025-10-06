2dfb01725a9c7c65408040c085bcf6adcade1700
Raphdon832 <50739318+Raphdon832@users.noreply.github.com>
2025-10-06 17:56:23 +0100

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -42,8 +42,8 @@ const Layout = ({ children }) => {
     <div className="min-h-screen bg-black flex flex-col">
       <Header />
       
-      {/* Desktop Sidebar - Hidden on mobile/tablet, visible on lg+ screens */}
-      <aside className="hidden lg:flex lg:flex-col fixed left-0 top-16 bottom-0 w-64 bg-dark-800 border-r border-dark-700 z-30">
+      {/* Desktop Sidebar - Hidden on mobile, visible on tablet/desktop (md+) screens */}
+      <aside className="hidden md:flex md:flex-col fixed left-0 top-16 bottom-0 w-64 bg-dark-800 border-r border-dark-700 z-30">
         <div className="flex-1 overflow-y-auto py-6">
           {/* Main Navigation */}
           <nav className="px-3 space-y-1">
@@ -142,13 +142,13 @@ const Layout = ({ children }) => {
         </div>
       </aside>
       
-      {/* Main Content - Add left padding on desktop to account for sidebar */}
-      <main className="flex-1 overflow-y-auto bg-black pt-16 pb-24 lg:pl-64 lg:pb-6">
+      {/* Main Content - Add left padding on tablet/desktop to account for sidebar */}
+      <main className="flex-1 overflow-y-auto bg-black pt-16 pb-24 md:pl-64 md:pb-6">
         {children}
       </main>
       
-      {/* Bottom Navigation - Hidden on desktop */}
-      <div className="lg:hidden">
+      {/* Bottom Navigation - Hidden on tablet/desktop */}
+      <div className="md:hidden">
         <BottomNavigation />
       </div>
     </div>
